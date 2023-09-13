b6edafe100b3a97250512f51952cebeecf8f4686
Edgar <jorge260590@hotmail.com>
2023-09-13 12:43:21 -0500

    feat(JavaScript, fix, style): Add JavaScript functions and events, Fix archive names and styles classes

---
+++ src/JS/App.js
@@ -0,0 +1,19 @@
+const circleRadius = document.querySelectorAll('[data-circle]');
+const SUBMIT = document.getElementById('btnSubmit');
+let selectedCircle = null;
+
+//checker to print in console and selection with color
+const checker = e => {
+    circleRadius.forEach(circle => circle.classList.remove('active'));
+
+    const clickedCircle = e.target;
+    clickedCircle.classList.add('active');
+
+    console.log(e)
+};
+
+//event to each circle
+circleRadius.forEach(circle => circle.addEventListener('click', checker));
+
+//event to send a thanks message
+SUBMIT.addEventListener('click', () => window.location.href = 'thanks.html');
